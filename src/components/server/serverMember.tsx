0274d01dfa543dac46b45c0f3fce2930b35e01f0
JoshHyde9 <joshhyde546@gmail.com>
2024-04-26 11:12:56 +1000

    feat: added click handler to message server member

--- src/components/server/serverMember.tsx
+++ src/components/server/serverMember.tsx
@@ -2,7 +2,7 @@
 
 import type { Member, Server, User } from "@prisma/client";
 
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { cn } from "~/lib/utils";
 import { roleIconMap } from "~/lib/iconMaps";
@@ -13,11 +13,22 @@ type ServerMemberProps = {
   server: Server;
 };
 
+type ParamsProps = {
+  memberId?: string;
+  serverId?: string;
+};
+
 export const ServerMember = ({ member }: ServerMemberProps) => {
-  const params = useParams<{ memberId?: string }>();
+  const params = useParams<ParamsProps>();
+  const router = useRouter();
+
+  const onMessageMember = () => {
+    router.push(`/server/${params?.serverId}/conversation/${member.id}`);
+  };
 
   return (
     <button
+      onClick={onMessageMember}
       className={cn(
         "group mb-1 flex w-full items-center gap-x-2 rounded-md p-2 transition hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50",
         params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700",
@@ -27,7 +38,7 @@ export const ServerMember = ({ member }: ServerMemberProps) => {
       <p
         className={cn(
           "text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300",
-          params.memberId === member.id &&
+          params?.memberId === member.id &&
             "text-primary dark:text-zinc-200 dark:group-hover:text-white",
         )}
       >
